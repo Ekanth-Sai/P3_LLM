7bbf37573e98dab3a3b111c8f1592fdc9a608803
Ekanth-Sai <ekanthsai.yellanki@gmail.com>
2025-07-16 15:34:21 +0530

    landing page frontend only

---
+++ src/app/landing/landing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrls: ['./landing.css']
+})
+
+export class LandingComponent {}
