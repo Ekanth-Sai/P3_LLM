c86eaf2a33d7ecea9d8909976216b3c46d391d5c
EkanthSaiFD <ekantha@firstdemat.in>
2025-10-16 23:49:13 +0530

    Documents display

--- frontend/src/app/services/document.service.ts
+++ frontend/src/app/services/document.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/admin/files';
+  private apiUrl = 'http://localhost:5001/processed-documents';
 
   getDocuments(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<string[]>(this.apiUrl).pipe(
+      map(paths => paths.map(path => ({ filename: path.split('/').pop() })))
+    );
   }
 }
